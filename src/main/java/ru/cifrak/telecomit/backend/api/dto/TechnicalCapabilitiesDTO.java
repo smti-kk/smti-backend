ed5072e28a61ac3303ea96ed3daad96b957ef54f
Panov Aleksander <a.panov@sysolutions.ru>
2020-04-23 09:15:03 +0700

    rafactor TechnicalCapabilitiesDTO using another LocDTO

--- src/main/java/ru/cifrak/telecomit/backend/api/dto/TechnicalCapabilitiesDTO.java
+++ src/main/java/ru/cifrak/telecomit/backend/api/dto/TechnicalCapabilitiesDTO.java
@@ -1,11 +1,13 @@
 package ru.cifrak.telecomit.backend.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import ru.cifrak.telecomit.backend.domain.*;
 
 import java.util.Set;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TechnicalCapabilitiesDTO {
     private Set<FtcInternet> internet;
     private Set<FtcAts> ats;
@@ -13,7 +15,7 @@ public class TechnicalCapabilitiesDTO {
     private Set<FtcPost> post;
     private Set<FtcRadio> radio;
     private Set<FtcTelevision> television;
-    private LocationSimple location;
+    private LocationTcDTO location;
 
     public TechnicalCapabilitiesDTO(CatalogsLocation location) {
         this.internet = location.getFtcInternets();
@@ -22,6 +24,6 @@ public class TechnicalCapabilitiesDTO {
         this.post = location.getFtcPosts();
         this.radio = location.getFtcRadios();
         this.television = location.getFtcTelevisions();
-        this.location = new LocationSimple(location);
+        this.location = new LocationTcDTO(location);
     }
 }
