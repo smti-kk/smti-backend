d346f25ae870b437a8f62940ab62e282aa5279e3
Panov Aleksander <a.panov@sysolutions.ru>
2020-04-27 11:15:43 +0700

    [api] ApiMobileType

--- src/main/java/ru/cifrak/telecomit/backend/api/ApiMobileType.java
+++ src/main/java/ru/cifrak/telecomit/backend/api/ApiMobileType.java
@@ -6,28 +6,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.cifrak.telecomit.backend.domain.CatalogsInternetaccesstype;
 import ru.cifrak.telecomit.backend.domain.CatalogsMobiletype;
-import ru.cifrak.telecomit.backend.domain.CatalogsSmotype;
 import ru.cifrak.telecomit.backend.repository.RepositoryInternetAccessType;
-import ru.cifrak.telecomit.backend.repository.RepositorySmoType;
+import ru.cifrak.telecomit.backend.repository.RepositoryMobileType;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/internet-access-type")
-public class ApiInternetAccessType {
-    private RepositoryInternetAccessType repository;
+@RequestMapping("/api/v1/mobile-type")
+public class ApiMobileType {
+    private RepositoryMobileType repository;
 
-    public ApiInternetAccessType(RepositoryInternetAccessType repository) {
+    public ApiMobileType(RepositoryMobileType repository) {
         this.repository = repository;
     }
 
     @GetMapping
-    public List<CatalogsInternetaccesstype> list(){
+    public List<CatalogsMobiletype> list(){
         return repository.findAll();
     }
 
     @GetMapping("/{id}/")
-    public CatalogsInternetaccesstype one(@PathVariable Integer id){
+    public CatalogsMobiletype one(@PathVariable Integer id){
         return repository.findById(id).orElse(null);
     }
+
 }
