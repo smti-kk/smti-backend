c303e4f2cd0556a932639b87e9e77a493f7be95a
Panov Aleksander <a.panov@sysolutions.ru>
2020-05-01 18:25:50 +0700

    :construction: added GovProg ans Signal Type and migrations

---
+++ src/main/java/ru/cifrak/telecomit/backend/entities/Signal.java
@@ -0,0 +1,34 @@
+package ru.cifrak.telecomit.backend.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum Signal {
+    ATV(1, "Аналоговое"),
+    CTV(2, "Цифровое");
+
+    private Integer id;
+    private String name;
+
+    Signal(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
