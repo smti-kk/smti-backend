38e6ce5ab03b0c3b5a6d44d3ccaa3f6c25cb5c40
Panov Aleksander <a.panov@sysolutions.ru>
2020-05-07 17:07:51 +0700

    :bug: data migrations on types from prod data

--- src/main/resources/db/changelog/0.0.1/data/operators.sql
+++ src/main/resources/db/changelog/0.0.1/data/operators.sql
@@ -1,10 +1,10 @@
-INSERT INTO operator (name,contacts,juristic_name,icon,services,inn,kpp,"type") VALUES
-('Билайн','','Вымпелком','beeline.png','internet,mobile,tv',NULL,NULL,NULL)
-,('Мегафон','','Мегафон','megafon.png','internet,mobile',NULL,NULL,NULL)
-,('Теле2','','Теле2','tele2.png','internet,mobile',NULL,NULL,NULL)
-,('МТС','','Публичное акционерное общество «Мобильные ТелеСистемы»','mts.png','internet,mobile',NULL,NULL,NULL)
-,('Ростелеком','','Ростелеком','rtk.png','ats,internet,tv,radio',NULL,NULL,NULL)
-,('СибТТК','','СибТТК','sibttk.png','internet',NULL,NULL,NULL)
-,('Искра','','Искра','iskra.png','ats,internet,mobile',NULL,NULL,NULL)
-,('РТРС','','РТРС','rtrs.png','tv,radio',NULL,NULL,NULL)
+INSERT INTO operator (id,"name",contacts,juristic_name,icon,services,inn,kpp,"type") VALUES
+(3,'Билайн','','Вымпелком','beeline.png','internet,mobile',NULL,NULL,NULL)
+,(4,'Мегафон','','Мегафон','megafon.png','internet,mobile',NULL,NULL,NULL)
+,(5,'Теле2','','Теле2','tele2.png','internet,mobile',NULL,NULL,NULL)
+,(6,'МТС','','Публичное акционерное общество «Мобильные ТелеСистемы»','mts.png','internet,mobile',NULL,NULL,NULL)
+,(7,'РТРС','','РТРС','rtrs.png','tv,radio',NULL,NULL,NULL)
+,(8,'СибТТК','','СибТТК','sibttk.png','internet',NULL,NULL,NULL)
+,(9,'Ростелеком','','Ростелеком','rtk.png','ats,internet,mobile',NULL,NULL,NULL)
+,(10,'Искра','','Искра','iskra.png','internet',NULL,NULL,NULL)
 ;
