c10fdb4722531e9ce3a7cdfd5eec460b07a817db
Panov Aleksander <a.panov@sysolutions.ru>
2020-05-27 12:19:25 +0700

    :recycle: [api] Organization MORE location

--- src/main/java/ru/cifrak/telecomit/backend/api/dto/OrganizationDTO.java
+++ src/main/java/ru/cifrak/telecomit/backend/api/dto/OrganizationDTO.java
@@ -1,14 +1,10 @@
 package ru.cifrak.telecomit.backend.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.cifrak.telecomit.backend.entities.Organization;
-import ru.cifrak.telecomit.backend.entities.TypeOrganization;
-import ru.cifrak.telecomit.backend.entities.TypeSmo;
 
-import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -27,7 +23,7 @@ public class OrganizationDTO {
     private Integer parent;
     private TypeOrgDTO type;
     private TypeSmoDTO smo;
-    private Integer location;
+    private LocationDTO location;
 
     public OrganizationDTO(Organization organization) {
         this.id = organization.getId();
@@ -40,6 +36,6 @@ public class OrganizationDTO {
         this.parent = organization.getParent() != null ? organization.getParent().getId() : null;
         this.type = organization.getType() != null ? new TypeOrgDTO(organization.getType()) : null;
         this.smo = organization.getSmo() != null ? new TypeSmoDTO(organization.getSmo()) : null;
-        this.location = organization.getLocation() != null ? organization.getLocation().getId() : null;
+        this.location = organization.getLocation() != null ? new LocationDTO(organization.getLocation()) : null;
     }
 }
