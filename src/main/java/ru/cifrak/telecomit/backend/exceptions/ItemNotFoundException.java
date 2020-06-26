a1d7feb56b13c3d7fd41d7f857d9fac6ef397dd1
v.turkov <v.turkov@sysolutions.ru>
2020-06-26 14:47:09 +0700

    added crud base stations

---
+++ src/main/java/ru/cifrak/telecomit/backend/exceptions/ItemNotFoundException.java
@@ -0,0 +1,19 @@
+package ru.cifrak.telecomit.backend.exceptions;
+
+public class ItemNotFoundException extends RuntimeException {
+    public ItemNotFoundException(Integer id) {
+        super("Item not found: " + id);
+    }
+    public ItemNotFoundException(Long id) {
+        super("Item not found: " + id);
+    }
+
+    public ItemNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ItemNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+}
