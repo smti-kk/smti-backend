d562988ac4b5296ffc6479aa184301aa432306f7
Panov Aleksander <a.panov@sysolutions.ru>
2020-07-21 10:50:21 +0700

    761 [api] added roles changes

--- src/main/java/ru/cifrak/telecomit/backend/api/ApiUser.java
+++ src/main/java/ru/cifrak/telecomit/backend/api/ApiUser.java
@@ -3,16 +3,11 @@ package ru.cifrak.telecomit.backend.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.cifrak.telecomit.backend.api.dto.OrganizationDTO;
-import ru.cifrak.telecomit.backend.api.dto.OrganizationShortDTO;
 import ru.cifrak.telecomit.backend.auth.repository.RepositoryAccount;
 import ru.cifrak.telecomit.backend.auth.repository.RepositoryUser;
 import ru.cifrak.telecomit.backend.entities.Account;
-import ru.cifrak.telecomit.backend.entities.Organization;
-import ru.cifrak.telecomit.backend.entities.User;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -32,8 +27,9 @@ public class ApiUser {
     }
 
     @PutMapping(consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Account> update(@RequestBody Account value){
+    public ResponseEntity<Account> update(@RequestBody Account value) {
         log.info("->PUT /api/user/::{}", value.getId());
+        //TODO:[generate TICKET]: null check
         Account item = rAccount.findById(value.getId()).get();
         item.setEmail(value.getEmail());
         item.setFirstName(value.getFirstName());
@@ -41,6 +37,7 @@ public class ApiUser {
         item.setPatronymicName(value.getPatronymicName());
         item.setLocations(value.getLocations());
         item.setIsActive(value.getIsActive());
+        item.setRoles(value.getRoles());
         item = rAccount.save(item);
         log.info("<-PUT /api/user/::{}", value.getId());
         return ResponseEntity.ok(item);
