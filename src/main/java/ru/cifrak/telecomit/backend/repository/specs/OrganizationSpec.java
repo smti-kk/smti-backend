32f9224512071e22246d2719288d1e7abd8a830c
Tolstonogov Alexey <tolstonogovav@cifra-k.ru>
2020-09-14 12:18:45 +0700

    feature/1358: WIP

--- src/main/java/ru/cifrak/telecomit/backend/repository/specs/OrganizationSpec.java
+++ src/main/java/ru/cifrak/telecomit/backend/repository/specs/OrganizationSpec.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.domain.Specification;
 import ru.cifrak.telecomit.backend.entities.*;
 import ru.cifrak.telecomit.backend.entities.Organization_;
 
+import java.util.List;
+
 public class OrganizationSpec {
     public static Specification<Organization> inLocation(Location location) {
         return (root, cq, cb) -> cb.equal(root.get(Organization_.location), location);
@@ -15,5 +17,21 @@ public class OrganizationSpec {
 
     public static Specification<Organization> withSmo(TypeSmo smo) {
         return (root, cq, cb) -> cb.equal(root.get(Organization_.smo), smo);
+    }
+
+    public static Specification<Organization> withOrgname(String orgname) {
+        return (root, cq, cb) -> cb.like(cb.lower(root.get(Organization_.name)), "%" + orgname.toLowerCase() + "%");
+    }
+
+    public static Specification<Organization> inParent(List<Location> parents) {
+        return (root, cq, cb) -> cb.and(root.get(Organization_.location).get(Location_.parent).in(parents));
+    }
+
+    public static Specification<Organization> pStart(Integer pStart) {
+        return (root, cq, cb) -> cb.and(cb.greaterThanOrEqualTo(root.get(Organization_.location).get(Location_.population), (pStart)));
+    }
+
+    public static Specification<Organization> pEnd(Integer pEnd) {
+        return (root, cq, cb) -> cb.and(cb.lessThanOrEqualTo(root.get(Organization_.location).get(Location_.population), (pEnd)));
     }
 }
