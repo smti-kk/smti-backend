d7d3313ffb20726a0d4fdcb8dda33e8170cd69bc
Vitaliy Turkov <v.turkov@sysolutions.ru>
2020-10-08 13:39:29 +0700

    added filters

--- src/main/java/ru/cifrak/telecomit/backend/base/station/repository/BaseStationsRepository.java
+++ src/main/java/ru/cifrak/telecomit/backend/base/station/repository/BaseStationsRepository.java
@@ -1,19 +1,29 @@
 package ru.cifrak.telecomit.backend.base.station.repository;
 
+import com.querydsl.core.types.dsl.EntityPathBase;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.data.jpa.repository.EntityGraph;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import ru.cifrak.telecomit.backend.base.station.entity.BaseStation;
+import ru.cifrak.telecomit.backend.base.station.entity.QBaseStation;
 import ru.cifrak.telecomit.backend.entities.Operator;
 import ru.cifrak.telecomit.backend.entities.TypeMobile;
-
-import org.locationtech.jts.geom.Point;
+import ru.cifrak.telecomit.backend.entities.locationsummary.QLocationForTable;
 
 import java.util.List;
 import java.util.Optional;
 
 
-public interface BaseStationsRepository extends JpaRepository<BaseStation, Integer> {
+public interface BaseStationsRepository extends JpaRepository<BaseStation, Integer>,
+        QuerydslPredicateExecutor<BaseStation>, QuerydslBinderCustomizer<EntityPathBase<QBaseStation>> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, EntityPathBase<QBaseStation> root) {
+    }
+
     @Override
     @NotNull
     List<BaseStation> findAll();
