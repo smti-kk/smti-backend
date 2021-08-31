cc0b1832a6c33892028cc49829370ae7c59c28ad
tolstonogovav <tolstonogovav@cifra-k.ru>
2021-08-31 12:03:24 +0700

    3644: journal - splitting by lines multiple changes and filtering by parameters

--- src/main/java/ru/cifrak/telecomit/backend/repository/RepositoryFeaturesRequests.java
+++ src/main/java/ru/cifrak/telecomit/backend/repository/RepositoryFeaturesRequests.java
@@ -21,11 +21,13 @@ public interface RepositoryFeaturesRequests extends JpaRepository<LocationFeatur
             "AND r.comment not like '%Импорт%' " +
             "ORDER BY r.created DESC")
     Page<LocationFeaturesEditingRequestFull> findAllRequests(Pageable pageable);
+
     @Query("FROM LocationFeaturesEditingRequestFull r " +
             "WHERE r.status <> 'DECLINED' " +
             "AND r.status <> 'WAIT_FOR_STATE_TO_BE_SET' " +
             "ORDER BY r.created DESC")
     Page<LocationFeaturesEditingRequestFull> findAllRequestsAndImportAndEditions(Pageable pageable);
+
     List<LocationFeaturesEditingRequestFull> findAllByLocationIdOrderByCreatedDesc(Integer locationId);
     Page<LocationFeaturesEditingRequestFull> findAllByUserOrderByCreatedDesc(User user, Pageable pageable);
 
---
+++ src/main/java/ru/cifrak/telecomit/backend/utils/SortingOrder.java
@@ -0,0 +1,6 @@
+package ru.cifrak.telecomit.backend.utils;
+
+public enum SortingOrder {
+    ASC,
+    DESC
+}
