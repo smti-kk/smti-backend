59f4c25255737e9ede403032099d674ee6d086a8
shih <mrshih1256@gmail.com>
2022-12-01 17:45:33 +0700

    Recreate view /with-quality

--- src/main/java/ru/cifrak/telecomit/backend/api/dto/DtoMapLocation.java
+++ src/main/java/ru/cifrak/telecomit/backend/api/dto/DtoMapLocation.java
@@ -12,29 +12,29 @@ import java.io.Serializable;
 @Immutable
 @Table(name = "view_map_location")
 @Subselect("SELECT row_number() over () as id,\n" +
-        "                    l.id as location_id,\n" +
-        "                   l.fias as fias,\n" +
-        "                   l.level as level,\n" +
-        "                   l.name as name,\n" +
-        "                   l.okato as okato,\n" +
-        "                   l.oktmo as oktmo,\n" +
-        "                   l.population as population,\n" +
-        "                   l.type as type,\n" +
-        "                   gd.id as geo_id,\n" +
-        "                   gd.administrative_center as administrative_center,\n" +
-        "                   l.parent_id as parent_id,\n" +
-        "                   p.name as parent_name,\n" +
-        "                   p.type as parent_type,\n" +
-        "                   p.level as parent_level,\n" +
-        "                   string_agg(tc.quality, ', ') as qualities\n" +
-        "            FROM location l\n" +
-        "                     inner join technical_capability tc on l.id = tc.key_location\n" +
-        "                     inner join geo_data gd on gd.id = l.geo_data_id\n" +
-        "                     inner join location p on p.id = l.parent_id\n" +
-        "            where l.type not in ('р-н', 'край', 'с/с', 'тер', 'мо')\n" +
-        "              and l.geo_data_id is not null\n" +
-        "              and tc.split = 'MOBILE'\n" +
-        "            group by l.id, gd.id, p.id")
+        "       l.id as location_id,\n" +
+        "       l.fias as fias,\n" +
+        "       l.level as level,\n" +
+        "       l.name as name,\n" +
+        "       l.okato as okato,\n" +
+        "       l.oktmo as oktmo,\n" +
+        "       l.population as population,\n" +
+        "       l.type as type,\n" +
+        "       gd.id as geo_id,\n" +
+        "       gd.administrative_center as administrative_center,\n" +
+        "       l.parent_id as parent_id,\n" +
+        "       p.name as parent_name,\n" +
+        "       p.type as parent_type,\n" +
+        "       p.level as parent_level,\n" +
+        "       (select string_agg(tc.quality, ', ') from technical_capability tc where tc.split = 'MOBILE' and tc.key_location = l.id) as qualities\n" +
+        "FROM location l\n" +
+        "         inner join technical_capability tc on l.id = tc.key_location\n" +
+        "         inner join geo_data gd on gd.id = l.geo_data_id\n" +
+        "         inner join location p on p.id = l.parent_id\n" +
+        "where l.type not in ('р-н', 'край', 'с/с', 'тер', 'мо')\n" +
+        "  and l.geo_data_id is not null\n" +
+        "group by l.id, gd.id, p.id\n" +
+        "order by l.id")
 
 public class DtoMapLocation implements Serializable {
     @Id
